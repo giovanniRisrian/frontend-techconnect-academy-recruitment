6fa3f5ff102b50637a25fdad0ba2e4cbc4e7b8d1
Zahrul Zizki Dinanto <zahrulz11pro@gmail.com>
2022-02-25 21:02:07 +0700

    Login UI

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,24 @@
-import Login from "../globalComponent/login/Login"
-import { Navigate } from 'react-router-dom';
-const Logins = ()=>{
-return(<div>Ini Login <Login/></div>)
-}
-export default Logins
+import Login from "../globalComponent/login/Login";
+import { Navigate } from "react-router-dom";
+import Card from "@mui/material/Card";
+import MyComponent from "../homepage/BackgroundImage";
+const Logins = () => {
+  return (
+    <MyComponent
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card style={{ backgroundColor: 'transparent',height: "30vh", padding: "4vh" ,
+    border: "1px solid",
+    padding: "10px",
+    boxShadow: "5px 10px black"}} variant="outlined">
+        <Login />
+      </Card>{" "}
+    </MyComponent>
+  );
+};
+export default Logins;
