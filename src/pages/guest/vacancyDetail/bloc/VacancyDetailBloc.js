e640d0d46eabd38c79ad0804cb9b9caade518580
istiqlaliyyah24 <istiqlaliyyah24@gmail.com>
2022-02-26 19:44:19 +0700

    delete sweetalert on apply program

--- src/pages/guest/vacancyDetail/bloc/VacancyDetailBloc.js
+++ src/pages/guest/vacancyDetail/bloc/VacancyDetailBloc.js
@@ -1,49 +1,48 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router";
-import swal from 'sweetalert2'
+import swal from "sweetalert2";
 
 const VacancyDetailBloc = (programService) => {
   let params = useParams();
   let navigate = useNavigate();
-  const[programDetail, setProgramDetail]= useState({})
-  let {getDetailInformationProgram, applyProgram } = programService();
+  const [programDetail, setProgramDetail] = useState({});
+  let { getDetailInformationProgram, applyProgram } = programService();
 
   const getProgrambyId = async () => {
     try {
       const response = await getDetailInformationProgram(params.id);
-      setProgramDetail(response.data)
-      return programDetail
+      setProgramDetail(response.data);
+      return programDetail;
     } catch (err) {
       throw err;
     }
   };
-  const doApplyProgram = async (values) =>{
-    console.log("data apply",values);
-      try{
-        // const config = {
-        //   headers: { Authorization: `Bearer ${context.userInfo}` },
-        // };
-        let res = await applyProgram(values);
-        swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'Success',
-          showConfirmButton: false,
-          timer: 1500
-        })
-        navigate("/applicant/status")
-        return res
-      }catch(err){
-        throw(err)
-      }
-  }
-  
+  const doApplyProgram = async (values) => {
+    console.log("data apply", values);
+    try {
+      // const config = {
+      //   headers: { Authorization: `Bearer ${context.userInfo}` },
+      // };
+      let res = await applyProgram(values);
+     
+      navigate("/applicant/status");
+      return res;
+    } catch (err) {
+      swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+      throw err;
+    }
+  };
+
   return {
     programDetail,
     params,
     navigate,
     getProgrambyId,
-    doApplyProgram
+    doApplyProgram,
   };
 };
 
