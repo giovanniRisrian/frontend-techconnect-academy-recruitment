31c38f88e337ada7be46c950fe9f5791f1fe19d1
giovanniRisrian <giovanni.risrian@gmail.com>
2022-02-28 00:22:36 +0700

    implementing the correct path for api to hit

--- src/pages/applicant/accept reject logic/service/AcceptRejectService.js
+++ src/pages/applicant/accept reject logic/service/AcceptRejectService.js
@@ -3,12 +3,18 @@ import { client } from "../../../../http-client/Client";
 // route nya masih boleh disesuaikan
 const AcceptRejectService = () => {
   async function acceptApplicant(applicant) {
-    const response = await client.post("/profile", applicant);
+    const response = await client.post(
+      "/program_applicant/accepted",
+      applicant
+    );
     return response;
   }
 
   async function rejectApplicant(applicant) {
-    const response = await client.post("/profile", applicant);
+    const response = await client.post(
+      "/program_applicant/rejected",
+      applicant
+    );
     return response;
   }
   return { acceptApplicant, rejectApplicant };
