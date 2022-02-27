6218adadf8eab910646e61ab7cec27130f4d9b3a
giovanniRisrian <giovanni.risrian@gmail.com>
2022-02-27 23:40:15 +0700

    implementing accept and reject UI logic

--- src/pages/applicant/accept reject logic/bloc/AcceptRejectBloc.js
+++ src/pages/applicant/accept reject logic/bloc/AcceptRejectBloc.js
@@ -1,9 +1,29 @@
 // navigation diambil dari route navigation di folder routes
-const AcceptRejectBloc = (navigation) => {
-  let { navigateTo, paramsNav } = navigation();
+const AcceptRejectBloc = (profileRepository, navigation) => {
+  const { acceptApplicant, rejectApplicant } = profileRepository();
+  let { paramsNav } = navigation();
   let params = paramsNav();
+  let applicant = {
+    applicantId: params.applicantId,
+    programId: params.programId,
+  };
 
-  return <div></div>;
+  const handleAccept = async () => {
+    try {
+      await acceptApplicant(applicant);
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  const handleReject = async () => {
+    try {
+      await rejectApplicant(applicant);
+    } catch (e) {
+      throw e;
+    }
+  };
+  return { handleAccept, handleReject };
 };
 
 export default AcceptRejectBloc;
