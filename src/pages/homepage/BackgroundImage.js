d7580327d35bf873b85ab52c3ec3f59d8d73349e
giovanniRisrian <giovanni.risrian@gmail.com>
2022-03-03 19:45:42 +0700

    fixing template in css

--- src/pages/homepage/BackgroundImage.js
+++ src/pages/homepage/BackgroundImage.js
@@ -1,10 +1,10 @@
-import { styled } from '@mui/system';
-import background from '../../asset/image/background.jpg'
+import { styled } from "@mui/system";
+import background from "../../asset/image/background.jpg";
 
-const MyComponent = styled('div')({
-    backgroundImage: `url(${background})`,
-    backgroundSize:"cover",
-    minHeight:"85vh",
-  });
+const MyComponent = styled("div")({
+  backgroundImage: `url(${background})`,
+  backgroundSize: "cover",
+  minHeight: "100vh",
+});
 
 export default MyComponent;
